9b7df7d6db226a8dfa2ba40fe3b39abd0c17c099
Michael Romay <amerror@gmail.com>
2020-03-25 15:22:59 -0700

    data helpers

---
+++ db/data-helpers.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const connect = require('../lib/utils/connect');
+const seed = require('./seed');
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+beforeAll(() => {
+  connect();
+});
+
+beforeEach(() => {
+  return mongoose.connection.dropDatabase();
+});
+
+beforeEach(() => {
+  return seed();
+});
+
+afterAll(() => {
+  return mongoose.connection.close();
+});
+
+const prepare = model => JSON.parse(JSON.stringify(model));
+const prepareAll = models => models.map(prepare);
+
+// read from models directory
+const files = fs.readdirSync('./lib/models');
+const getters = files
+  .map(file => require(`../lib/models/${file}`))
+  .filter(Model => Model.prototype instanceof mongoose.Model)
+  .reduce((acc, Model) => {
+    return {
+      ...acc,
+      [`get${Model.modelName}`]: query => Model.findOne(query).then(prepare),
+      [`get${Model.modelName}s`]: query => Model.find(query).then(prepareAll)
+    };
+  }, {});
+
+module.exports = getters;
